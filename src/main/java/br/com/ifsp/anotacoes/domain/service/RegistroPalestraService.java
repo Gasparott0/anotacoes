53b57a0e62c023c785efb83d7fb52c0ca7d72f8b
Mateus da Silva Gasparotto <mateusg@aquarius.cpqd.com.br>
2020-09-27 16:30:22 -0300

    Inserção, listagem e procura de palestras

--- src/main/java/br/com/ifsp/anotacoes/domain/service/RegistroPalestraService.java
+++ src/main/java/br/com/ifsp/anotacoes/domain/service/RegistroPalestraService.java
@@ -6,26 +6,34 @@ import org.springframework.stereotype.Service;
 import br.com.ifsp.anotacoes.domain.exception.ServicoException;
 import br.com.ifsp.anotacoes.domain.model.Conta;
 import br.com.ifsp.anotacoes.domain.model.Evento;
+import br.com.ifsp.anotacoes.domain.model.Palestra;
 import br.com.ifsp.anotacoes.domain.repository.ContaRepository;
 import br.com.ifsp.anotacoes.domain.repository.EventoRepository;
+import br.com.ifsp.anotacoes.domain.repository.PalestraRepository;
 
 @Service
-public class RegistroEventoService {
+public class RegistroPalestraService {
 
+	@Autowired
+	private PalestraRepository palestraRepository;
+	
 	@Autowired
 	private ContaRepository contaRepository;
 
 	@Autowired
 	private EventoRepository eventoRepository;
 
-	public Evento salvar(Evento evento) {
+	public Palestra salvar(Palestra palestra) {
 
-		Conta conta = contaRepository.findById(evento.getConta().getId())
+		Conta conta = contaRepository.findById(palestra.getConta().getId())
+				.orElseThrow(() -> new ServicoException("Cliente não encontrado"));
+		
+		Evento evento = eventoRepository.findById(palestra.getEvento().getId())
 				.orElseThrow(() -> new ServicoException("Cliente não encontrado"));
 
-		evento.setConta(conta);
+		palestra.setConta(conta);
+		palestra.setEvento(evento);
 
-		return eventoRepository.save(evento);
+		return palestraRepository.save(palestra);
 	}
-
 }
---
+++ src/main/resources/db/migration/V003__create-table-palestra.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS palestra(
+	id BIGINT NOT NULL AUTO_INCREMENT,
+    evento_id BIGINT NOT NULL,
+    conta_id BIGINT NOT NULL,
+    nome VARCHAR(30),
+    data DATETIME NOT NULL,
+    
+    PRIMARY KEY (id),
+    
+    FOREIGN KEY (evento_id) REFERENCES evento(id),
+    FOREIGN KEY (conta_id) REFERENCES conta(id)
+);
